c82089fff5d67a98074707b9d85f0841f2a55225
Jayas <34701149+jayasio@users.noreply.github.com>
2020-01-28 14:48:29 +0100

    First commit

---
+++ Assets/Scripts/Tracker.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public struct Tracker
+{
+    public Vector3 position;
+    public Quaternion rotation;
+}
---
+++ Assets/Scripts/TrackingManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingManager : MonoBehaviour
+{
+    [SerializeField] GameObject Origin;
+    Tracker tracker;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
